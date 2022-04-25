49fc929ba387d82e294b6255d4c4f96c1be6422b
GabrielPRomero <80391539+GabrielPRomero@users.noreply.github.com>
2022-04-25 15:54:53 +0300

    started creating mergeSort file
    added commentary of the merge sort algorithm

---
+++ mergeSort.ts
@@ -0,0 +1,7 @@
+// mergesort is a recursive function that takes in an array and returns a sorted array
+// it works by splitting the array into two parts
+// then it calls itself on the two parts
+// then it merges the two parts 
+// it merges the two parts by comparing the elements
+// and swapping them if the first element is greater than the second element
+
